4b719c1dbab8598b1e4463b58f9ada7cd7af3839
diego.rios <diego.rios@CTNAPPLE01YG8WN.local>
2019-02-11 18:46:01 -0200

    first commit

--- caraOuCoroa/ViewController.swift
+++ caraOuCoroa/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let upSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(sender:)))
+        upSwipe.direction = .up
+        
+        
+        view.addGestureRecognizer(upSwipe)
     }
 
+    @objc func handleSwipe(sender: UISwipeGestureRecognizer) {
+        if sender.state == .ended {
+            switch sender.direction {
+                case .up:
+                    if random() {
+                        label.text = "Cara"
+                    }else{
+                        label.text = "Coroa"
+                    }
+                    break
+                default:
+                    break
+            }
+        }
+    }
+    
+    func random() -> Bool {
+        let stayOrGo = Bool.random()
+        return stayOrGo
+    }
 
 }
 
